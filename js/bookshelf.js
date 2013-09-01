9794dab9937f0c8b16b2cd27c912e5764c72d371
alexthompson288 <alexthompson288@gmail.com>
2013-09-01 16:23:49 +0100

    got artwork working dynmiacally

--- js/bookshelf.js
+++ js/bookshelf.js
@@ -5,18 +5,9 @@ $(document).ready(function(){
 	// $('.story-text a').click(activateReadingPad);
 	console.log('these are the word link elements object');
 	console.log($('#story-text-id h1 a'));
-	$('.book-cover-div a').click(addPageContentToStoryPageScreen);
-	$('next-page').click(addNextPageToScreen(currentPageNumber));
-	
+	$('.book-cover-div a').click(setStoryPageContent);
+
 });
-
-function addNextPageToScreen(currentPageNumber){
-		currentPageNumber = $('.next-page').data('pagenumber');
-		console.log('this is the new currentPageNumber');
-		console.log(currentPageNumber);
-		addPageContentToStoryPageScreen();
-}
-
 
 
 function drawBooksOntoBookshelf(){
@@ -24,14 +15,14 @@ function drawBooksOntoBookshelf(){
 	var storiesLength = stories.length - 1;
 	var i;
 	for(i = 0 ; i < storiesLength ; i++){
-	var story = stories[i];
-	console.log(story.id);
-	var html = '<div class="book-cover-div" id="book-' + story.id + '"><a href="#" data-story="'+ story.id +'"><img src="resources/storyart/book_cover_art/story_book_cover_' + story.id + '.png"></a></div>';
-	$('div.bookshelf-div-container').append(html);
+		var story = stories[i];
+		console.log(story.id);
+		var html = '<div class="book-cover-div" id="book-' + story.id + '"><a href="#" data-story="'+ story.id +'"><img src="resources/storyart/book_cover_art/story_book_cover_' + story.id + '.png"></a></div>';
+		$('div.bookshelf-div-container').append(html);
 	}
 }
 
-function addPageContentToStoryPageScreen(){
+function setStoryPageContent(){
 	// Find the right story when the book cover is clicked
 	var storyId = $(this).data('story');
 	var storiesLength = stories.length;
@@ -43,12 +34,12 @@ function addPageContentToStoryPageScreen(){
 		}
 	}
 	var story = story;
+	$('#story-page-id').data("currentstorynumber", storyId);
+	
 	// Find the matching story pages
 	// Find total number of storypage objects across all stories
 
 	var storyPagesLength = storyPageData.length-1;
-	console.log('This is storyPagesLength value');
-	console.log(storyPagesLength);
 	// Loop through all storypages objects and put all those with the same story_id into array called storyPages
 	var i;
 	var storyPages =[];
@@ -57,88 +48,140 @@ function addPageContentToStoryPageScreen(){
 			storyPages.push(storyPageData[i]);
 		}
 	}
-
 	// Log the result of that array
 	console.log('This is array of story pages');
 	console.log(storyPages);
 
 	var thisStoryPagesLength = storyPages.length;
-	console.log('This is the number of story pages for this story');
-	console.log(thisStoryPagesLength);
-	var firstPage = storyPages[0];
 	var currentPageNumber = 0;
+	console.log('this is current page number');
+	console.log(currentPageNumber);
+	var currentStoryPage = storyPages[currentPageNumber];
 	var nextPageNumber = currentPageNumber + 1;
 	var previousPageNumber = currentPageNumber - 1;
 
-	drawWordsToScreen(currentPageNumber);
-	drawBackgroundToScreen(currentPageNumber);
-	addLinksToScreen(currentPageNumber);
-	$('next-page').click(addNextPageToScreen);
 
 	
+	console.log('this is current page number');
+	console.log(currentPageNumber);
 
+	// Draw first page
+	drawAllPageContentToScreen(currentPageNumber);
 
+	// Draw subsequent pages
+	$('#next-page').click(function(){
+		var newPageNumber = $('#next-page').data('pagenumber');
+		console.log('new page number is...');
+		console.log(newPageNumber);
+		clearAllContentFromScreen();
+		drawAllPageContentToScreen(newPageNumber);
+		currentPageNumber = newPageNumber;
+		return currentPageNumber;
+	});
 
-	function addLinksToScreen(currentPageNumber){
-		// add next/previous buttons in the html
-		var linkHtmlNext = '<a href="#" id=" " data-pagenumber="'+nextPageNumber+'" class="next-page">next</a>';
-		var linkHtmlPrevious = '<a href="#" id=" " data-pagenumber="'+previousPageNumber+'" class="previous-page">next</a>';
+	currentPageNumber = currentPageNumber;
 
-		// Set the links with their data
+	
+	console.log('current page number is now ...');
+	console.log(currentPageNumber);
 
-		if(currentPageNumber < thisStoryPagesLength){
-			console.log('We need a next button!!');
-			$('.story-page').append(linkHtmlNext);
-		}
-		else if(currentPageNumber > 0){
-			console.log('We need a previous button!!');
-			$('.story-page').append(linkHtmlPrevious);
-		}
-		else if(currentPageNumber === thisStoryPagesLength){
-			console.log('DISPLAY END OF STORY PAGE');
-		}
-
-		// Create function to change currentPageNumber according to next/previous data attribute
-
-		function increaseCurrentPageNumberVariable(){
-			currentPageNumber = currentPageNumber + 1;
-			console.log('this is current page number plus one');
-			console.log(currentPageNumber);
-			return currentPageNumber;
-		}
-
-		function decreaseCurrentPageNumberVariable(){
-			currentPageNumber = currentPageNumber - 1;
-			console.log('this is current page number plus one');
-			console.log(currentPageNumber);
-			return currentPageNumber;
-		}
-
-		$('.next-page').click(increaseCurrentPageNumberVariable);
-
-		$('.previous-page').click(decreaseCurrentPageNumberVariable);
-
+	function increaseCurrentPageNumber(currentPageNumber){
+		currentPageNumber = currentPageNumber + 1;
+		console.log('current page number is...');
+		console.log(currentPageNumber);
+		return currentPageNumber;
 	}
 
-	// console.log('this is current page number plus one outside function when next link clicked');
-	// console.log(currentPageNumber);
-
-	// The sentence array number comes from currentPageNumber variable
-	function drawWordsToScreen(currentPageNumber){
-		var sentence = storyPages[currentPageNumber].text;
-		console.log('this is story object');
-		console.log(story);
-		console.log('this is the sentence');
-		console.log(sentence);
-		addLinkToAllWords(sentence);
-		$('.story-text').data('currentstorypagenumber', 'hello');
+	function logCurrentPageNumber(currentPageNumber){
+		alert('button clicked');
+		console.log(currentPageNumber);
 	}
 
-	function drawBackgroundToScreen(currentPageNumber){
-		var imagePath = 'resources/storyart/' + storyId + '_' + currentPageNumber + '.jpg';
-		console.log('This is the image path');
-		console.log(imagePath);
-		$('.story-page').css('background-image', 'url(' + imagePath + ')');
+	
+	// $('#next-page').click(function(){
+	// 	increaseCurrentPageNumber(currentPageNumber)
+	// 	return currentPageNumber;
+	// }
+	// );
+
+	// function increaseCurrentPageNumberVariable(currentPageNumber){
+	// 		currentPageNumber = currentPageNumber + 1;
+	// 		console.log('this is current page number plus one');
+	// 		console.log(currentPageNumber);
+	// 		return currentPageNumber;
+	// 	}
+
+	// function decreaseCurrentPageNumberVariable(currentPageNumber){
+	// 	currentPageNumber = currentPageNumber - 1;
+	// 	console.log('this is current page number plus one');
+	// 	console.log(currentPageNumber);
+	// 	return currentPageNumber;
+	// }
+
+	// $('.next-page').click(increaseCurrentPageNumberVariable(currentPageNumber));
+
+	// $('.previous-page').click(decreaseCurrentPageNumberVariable(currentPageNumber));
+	function clearAllContentFromScreen(){
+		$('.story-page h1').text('');
+		$('.story-text h1').empty();
+		$('#next-page').remove();
+	}
+	
+	function drawAllPageContentToScreen(currentPageNumber){
+		drawWordsToScreen(currentPageNumber);
+		drawBackgroundToScreen(currentPageNumber);
+		addLinksToScreen(currentPageNumber);
+		console.log('this is current page number');
+		console.log($('#story-text-id').data("currentpagenumber"));
+		function addLinksToScreen(currentPageNumber){
+			// add next/previous buttons in the html
+			var linkHtmlNext = '<a href="#" id="next-page" data-pagenumber="'+nextPageNumber+'" class="next-page">next</a>';
+			var linkHtmlPrevious = '<a href="#" id=" " data-pagenumber="'+previousPageNumber+'" class="previous-page">next</a>';
+
+			// Set the links with their data
+
+			if(currentPageNumber < thisStoryPagesLength){
+				console.log('We need a next button!!');
+				$('.story-page').append(linkHtmlNext);
+			}
+			else if(currentPageNumber > 0){
+				console.log('We need a previous button!!');
+				$('.story-page').append(linkHtmlPrevious);
+			}
+			else if(currentPageNumber === thisStoryPagesLength){
+				console.log('DISPLAY END OF STORY PAGE');
+			}
+
+			// Create function to change currentPageNumber according to next/previous data attribute
+
+		}
+
+		// console.log('this is current page number plus one outside function when next link clicked');
+		// console.log(currentPageNumber);
+
+		// The sentence array number comes from currentPageNumber variable
+		function drawWordsToScreen(currentPageNumber){
+			var sentence = storyPages[currentPageNumber].text;
+			addLinkToAllWords(sentence);
+			$('#story-text-id').data("currentpagenumber", currentPageNumber);
+			console.log('look here');
+			console.log($('.story-text').data("currentstorypagenumber"));
+			
+		}
+
+		function drawBackgroundToScreen(currentPageNumber){
+			// var imagePath = 'resources/storyart/' + storyId + '_' + currentPageNumber + '.jpg';
+			var htmlForImagesStart = 'url("';
+			var htmlForImagesEnd = '")';
+			currentStoryPage = storyPages[currentPageNumber];
+			var imagePath = 'resources/storyart/' + currentStoryPage.image;
+			var totalHtmlForImages = htmlForImagesStart + imagePath + htmlForImagesEnd;
+			console.log(totalHtmlForImages);
+			
+			console.log('This is the image path');
+			console.log('url(' + imagePath + ')');
+			$('.story-page').css('background-image', totalHtmlForImages);
+		}
 	}	
 	
 	
