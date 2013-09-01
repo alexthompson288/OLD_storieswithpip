9f581bf1c6541366bcd0b4c3c4a688dc4aef37cd
alexthompson288 <alexthompson288@gmail.com>
2013-09-01 14:04:21 +0100

    refactoring booksehlf.js to get it to change page

--- js/bookshelf.js
+++ js/bookshelf.js
@@ -2,9 +2,22 @@
 $(document).ready(function(){
 	$('.page-title-bg').fadeIn(3000);
 	drawBooksOntoBookshelf();
-	$('.story-text a').click(activateReadingPad);
-	$('.book-cover-div a').click(addContentToStoryPageScreen);
+	// $('.story-text a').click(activateReadingPad);
+	console.log('these are the word link elements object');
+	console.log($('#story-text-id h1 a'));
+	$('.book-cover-div a').click(addPageContentToStoryPageScreen);
+	$('next-page').click(addNextPageToScreen(currentPageNumber));
+	
 });
+
+function addNextPageToScreen(currentPageNumber){
+		currentPageNumber = $('.next-page').data('pagenumber');
+		console.log('this is the new currentPageNumber');
+		console.log(currentPageNumber);
+		addPageContentToStoryPageScreen();
+}
+
+
 
 function drawBooksOntoBookshelf(){
 	console.log(stories);
@@ -18,8 +31,8 @@ function drawBooksOntoBookshelf(){
 	}
 }
 
-function addContentToStoryPageScreen(){
-	// Identify the story object and its content when it is clicked
+function addPageContentToStoryPageScreen(){
+	// Find the right story when the book cover is clicked
 	var storyId = $(this).data('story');
 	var storiesLength = stories.length;
 	var i;
@@ -29,13 +42,8 @@ function addContentToStoryPageScreen(){
 			story = stories[i];
 		}
 	}
-	console.log(storyId);
-	console.log('This is the story');
-	console.log(story);
-	
-
-	// Find the appropriate sentence and display on screen
-
+	var story = story;
+	// Find the matching story pages
 	// Find total number of storypage objects across all stories
 
 	var storyPagesLength = storyPageData.length-1;
@@ -53,83 +61,97 @@ function addContentToStoryPageScreen(){
 	// Log the result of that array
 	console.log('This is array of story pages');
 	console.log(storyPages);
-	
-	// if currentPageNumber < total page number { display next }
-	// if currentPageNumber > 0 { display previous }
-	// if currentPageNumber === total page number { display end }
 
 	var thisStoryPagesLength = storyPages.length;
 	console.log('This is the number of story pages for this story');
 	console.log(thisStoryPagesLength);
+	var firstPage = storyPages[0];
 	var currentPageNumber = 0;
 	var nextPageNumber = currentPageNumber + 1;
 	var previousPageNumber = currentPageNumber - 1;
 
-	// add next/previous buttons in the html
-	var linkHtmlNext = '<a href="#" id=" " data-pagenumber="'+nextPageNumber+'" class="next-page">next</a>';
-	var linkHtmlPrevious = '<a href="#" id=" " data-pagenumber="'+previousPageNumber+'" class="previous-page">next</a>';
+	drawWordsToScreen(currentPageNumber);
+	drawBackgroundToScreen(currentPageNumber);
+	addLinksToScreen(currentPageNumber);
+	$('next-page').click(addNextPageToScreen);
 
-	// Set the links with their data
+	
 
-	if(currentPageNumber < thisStoryPagesLength){
-		console.log('We need a next button!!');
-		$('.story-page').append(linkHtmlNext);
+
+
+	function addLinksToScreen(currentPageNumber){
+		// add next/previous buttons in the html
+		var linkHtmlNext = '<a href="#" id=" " data-pagenumber="'+nextPageNumber+'" class="next-page">next</a>';
+		var linkHtmlPrevious = '<a href="#" id=" " data-pagenumber="'+previousPageNumber+'" class="previous-page">next</a>';
+
+		// Set the links with their data
+
+		if(currentPageNumber < thisStoryPagesLength){
+			console.log('We need a next button!!');
+			$('.story-page').append(linkHtmlNext);
+		}
+		else if(currentPageNumber > 0){
+			console.log('We need a previous button!!');
+			$('.story-page').append(linkHtmlPrevious);
+		}
+		else if(currentPageNumber === thisStoryPagesLength){
+			console.log('DISPLAY END OF STORY PAGE');
+		}
+
+		// Create function to change currentPageNumber according to next/previous data attribute
+
+		function increaseCurrentPageNumberVariable(){
+			currentPageNumber = currentPageNumber + 1;
+			console.log('this is current page number plus one');
+			console.log(currentPageNumber);
+			return currentPageNumber;
+		}
+
+		function decreaseCurrentPageNumberVariable(){
+			currentPageNumber = currentPageNumber - 1;
+			console.log('this is current page number plus one');
+			console.log(currentPageNumber);
+			return currentPageNumber;
+		}
+
+		$('.next-page').click(increaseCurrentPageNumberVariable);
+
+		$('.previous-page').click(decreaseCurrentPageNumberVariable);
+
 	}
-	else if(currentPageNumber > 0){
-		console.log('We need a previous button!!');
-		$('.story-page').append(linkHtmlPrevious);
-	}
-	else if(currentPageNumber === thisStoryPagesLength){
-		console.log('DISPLAY END OF STORY PAGE');
-	}
-
-	// Create function to change currentPageNumber according to next/previous data attribute
-
-	function increaseCurrentPageNumberVariable(){
-		currentPageNumber = currentPageNumber + 1;
-		console.log('this is current page number plus one');
-		console.log(currentPageNumber);
-		return currentPageNumber;
-	}
-
-	function decreaseCurrentPageNumberVariable(){
-		currentPageNumber = currentPageNumber - 1;
-		console.log('this is current page number plus one');
-		console.log(currentPageNumber);
-		return currentPageNumber;
-	}
-
-	$('.next-page').click(increaseCurrentPageNumberVariable);
-
-	$('.previous-page').click(decreaseCurrentPageNumberVariable);
 
 	// console.log('this is current page number plus one outside function when next link clicked');
 	// console.log(currentPageNumber);
 
 	// The sentence array number comes from currentPageNumber variable
+	function drawWordsToScreen(currentPageNumber){
+		var sentence = storyPages[currentPageNumber].text;
+		console.log('this is story object');
+		console.log(story);
+		console.log('this is the sentence');
+		console.log(sentence);
+		addLinkToAllWords(sentence);
+		$('.story-text').data('currentstorypagenumber', 'hello');
+	}
 
-	var sentence = storyPages[currentPageNumber].text;
-	console.log('this is story object');
-	console.log(story);
-	console.log('this is the sentence');
-	console.log(sentence);
-	addLinkToAllWords(sentence);
-
-	var imagePath = storyId + '_' + currentPageNumber + '.png';
-	console.log('This is the image path');
-	console.log(imagePath);
+	function drawBackgroundToScreen(currentPageNumber){
+		var imagePath = 'resources/storyart/' + storyId + '_' + currentPageNumber + '.jpg';
+		console.log('This is the image path');
+		console.log(imagePath);
+		$('.story-page').css('background-image', 'url(' + imagePath + ')');
+	}	
+	
+	
+	// Hide the bookshelf html upon click on a book
+	$('.bookshelf-container').fadeOut();
+	// Create the div template to add the story text into
+	$('.bookshelf-page .story-page').fadeIn();
 	
 
-	// Hide the bookshelf html upon click on a book
-
-	$('.bookshelf-container').hide();
-
-	// Create the div template to add the story text into
-
-	$('.bookshelf-page .story-page').show();
-
 	// CHANGE THE SYNTAX TO MAKE THIS WORK
-	$('.story-page').css('background','hello');
+
+	// This is where words get broken up and added to reading pad
+	$('.story-text a').click(activateReadingPad);
 
 	// Need to get sentence audio working dynamically
 	// Need to get text etc. to redraw upon next click based on data-attribute on the next/previous button
--- js/storyOneData.js
+++ js/storyOneData.js
@@ -37,5 +37,77 @@ var storyPageData = [
 "story_id":2,
 "text":"Pat sat as a tap.",
 "textposition":"topleft",
-"updated_at":"2013-07-25T09:16:43Z"}
+"updated_at":"2013-07-25T09:16:43Z"},
+
+{"audio":"module_1_unit_2_pat_sat_at_a_tap",
+"created_at":"2013-06-04T16:48:20Z",
+"id":20,
+"image":"unit 2_2.png",
+"pageorder":2,
+"story_id":1,
+"text":"Pat sat at the tap",
+"textposition":"topleft",
+"updated_at":"2013-07-25T09:15:09Z"},
+
+{"audio":"module_1_unit_2_pat_sat",
+"created_at":"2013-06-04T16:48:03Z",
+"id":19,
+"image":"unit 2_1.png",
+"pageorder":1,
+"story_id":1,
+"text":"Pat sat",
+"textposition":"topleft",
+"updated_at":"2013-07-25T09:14:46Z"},
+
+{"audio":"module_1_unit_4_nat_is_in_the_pit","created_at":"2013-06-04T16:51:36Z","id":25,"image":"unit 4_1.png","pageorder":1,"story_id":3,"text":"Nat is in the pit.","textposition":"topleft","updated_at":"2013-07-25T09:22:41Z"},
+
+{"audio":"module_1_unit_4_a_pin_is_in_the_pit","created_at":"2013-06-04T16:52:16Z","id":26,"image":"unit 4_2.png","pageorder":2,"story_id":3,"text":"A pin is in the pit.","textposition":"topcenter","updated_at":"2013-07-25T09:23:09Z"},
+
+{"audio":"module_1_unit_4_pat_is_in_the_pit","created_at":"2013-06-04T16:54:43Z","id":29,"image":"unit 4_5.png","pageorder":5,"story_id":3,"text":"Pat is in the pit.","textposition":"topleft","updated_at":"2013-07-25T09:24:09Z"},
+
+{"audio":"module_1_unit_4_pat_and_nat_nap_at_the_pit","created_at":"2013-06-04T16:55:15Z","id":30,"image":"unit 4_6.png","pageorder":6,"story_id":3,"text":"Pat and Nat nap at the pit.","textposition":"topleft","updated_at":"2013-07-25T09:24:44Z"},
+
+{"audio":"module_1_unit_4_nat_is_at_the_pin_in_the_pit","created_at":"2013-06-04T16:52:34Z","id":27,"image":"unit 4_3.png","pageorder":3,"story_id":3,"text":"Nat is at the pin in the pit.","textposition":"topcenter","updated_at":"2013-07-25T09:23:28Z"},
+
+{"audio":"module_1_unit_4_the_pin_is_in_nat","created_at":"2013-06-04T16:54:03Z","id":28,"image":"unit 4_4.png","pageorder":4,"story_id":3,"text":"The pin is in Nat.","textposition":"topleft","updated_at":"2013-07-25T09:23:44Z"},
+
+{"audio":"module_1_unit_5_sid_is_a_man_and_tim_is_a_pan","created_at":"2013-06-04T16:59:46Z","id":31,"image":"unit 5_1.png","pageorder":1,"story_id":4,"text":"Sid is a man\\n\r\nAnd Tim is a pan.","textposition":"topright","updated_at":"2013-07-25T09:25:23Z"},
+
+{"audio":"module_1_unit_5_sid_is_a_dad_and_tim_is_sad","created_at":"2013-06-04T17:01:18Z","id":32,"image":"unit 5_2.png","pageorder":2,"story_id":4,"text":"Sid is a dad\\n\r\nAnd Tim is sad.","textposition":"topright","updated_at":"2013-07-25T09:26:45Z"},
+
+{"audio":"module_1_unit_5_sid_is_mad_and_tim_is_sad","created_at":"2013-06-04T17:01:40Z","id":33,"image":"unit 5_3.png","pageorder":3,"story_id":4,"text":"Sid is mad\\n\r\nAnd Tim is sad.","textposition":"topright","updated_at":"2013-07-25T09:27:20Z"},
+
+{"audio":"module_1_unit_5_sid_is_in_a_din_and_tim_the_pan_is_tin","created_at":"2013-06-04T17:02:24Z","id":34,"image":"unit 5_4.png","pageorder":4,"story_id":4,"text":"Sid is in a din\\n\r\nAnd Tim the pan is tin.","textposition":"topright","updated_at":"2013-07-25T09:28:05Z"},
+
+{"audio":"module_1_unit_5_voice_over_2","created_at":"2013-06-04T17:02:55Z","id":35,"image":"unit 5_5.png","pageorder":5,"story_id":4,"text":"","textposition":"topright","updated_at":"2013-07-25T09:28:36Z"},
+
+{"audio":"module_1_unit_6_the_gas_got_in_the_dog","created_at":"2013-05-24T13:54:51Z","id":9,"image":"unit_6_part_8.png","pageorder":9,"story_id":5,"text":"The gas got in the dog.","textposition":"topright","updated_at":"2013-07-25T10:16:44Z"},
+
+{"audio":"","created_at":"2013-05-24T13:56:19Z","id":11,"image":"unit_6_part_10.png","pageorder":11,"story_id":5,"text":"","textposition":"","updated_at":"2013-07-25T10:16:59Z"},
+
+{"audio":"module_1_unit_6_the_pig_sat_on_the_pin","created_at":"2013-05-24T13:53:04Z","id":6,"image":"unit_6_part_6.png","pageorder":6,"story_id":5,"text":"The pig sat on the pin.","textposition":"topright","updated_at":"2013-07-25T10:16:15Z"},
+
+{"audio":"","created_at":"2013-05-24T14:00:14Z","id":15,"image":"unit_6_part_13.png","pageorder":15,"story_id":5,"text":"","textposition":"","updated_at":"2013-07-25T10:17:32Z"},
+
+{"audio":"","created_at":"2013-05-24T13:54:13Z","id":8,"image":"unit_6_part_7.png","pageorder":8,"story_id":5,"text":"","textposition":"","updated_at":"2013-07-25T10:16:33Z"},{"audio":"module_1_unit_6_pop_3","created_at":"2013-05-24T13:59:07Z","id":14,"image":"unit_6_part_12.png","pageorder":14,"story_id":5,"text":"Pop!","textposition":"topleft","updated_at":"2013-07-25T10:17:24Z"},
+
+{"audio":"","created_at":"2013-05-24T14:04:21Z","id":17,"image":"unit_6_part_15.png","pageorder":17,"story_id":5,"text":"","textposition":"","updated_at":"2013-07-25T10:17:47Z"},{"audio":"module_1_unit_6_pop_6","created_at":"2013-05-24T14:04:59Z","id":18,"image":"unit_6_part_16.png","pageorder":18,"story_id":5,"text":"Pop!","textposition":"topleft","updated_at":"2013-07-25T10:17:55Z"},
+
+{"audio":"","created_at":"2013-05-24T13:55:25Z","id":10,"image":"unit_6_part_9.png","pageorder":10,"story_id":5,"text":"","textposition":"","updated_at":"2013-07-25T10:16:51Z"},{"audio":"module_1_unit_6_pop","created_at":"2013-05-24T13:53:40Z","id":7,"image":"unit_6_part_5.png","pageorder":7,"story_id":5,"text":"Pop!","textposition":"topleft","updated_at":"2013-07-25T10:16:24Z"},
+
+{"audio":"module_1_unit_6_pop","created_at":"2013-05-24T13:48:00Z","id":1,"image":"unit_6_part_1.png","pageorder":1,"story_id":5,"text":"Pop!","textposition":"topright","updated_at":"2013-07-25T10:14:16Z"},
+
+{"audio":"","created_at":"2013-05-24T13:51:37Z","id":4,"image":"unit_6_part_4.png","pageorder":4,"story_id":5,"text":"","textposition":"","updated_at":"2013-07-25T10:15:10Z"},
+
+{"audio":"","created_at":"2013-05-24T13:50:18Z","id":3,"image":"unit_6_part_3.png","pageorder":3,"story_id":5,"text":"","textposition":"","updated_at":"2013-07-25T10:14:48Z"},
+
+{"audio":"module_1_unit_6_the_gas_got_in_the_pig","created_at":"2013-05-24T13:49:33Z","id":2,"image":"unit_6_part_2.png","pageorder":2,"story_id":5,"text":"The gas got in the pig.","textposition":"topright","updated_at":"2013-07-25T10:14:31Z"},
+
+{"audio":"module_1_unit_6_the_pig_and_the_dog_got_on_the_man","created_at":"2013-05-24T14:02:45Z","id":16,"image":"unit_6_part_14.png","pageorder":16,"story_id":5,"text":"The pig and the dog got on the man.","textposition":"topleft","updated_at":"2013-08-06T12:05:02Z"},
+
+{"audio":"module_1_unit_6_pop_2","created_at":"2013-05-24T13:52:28Z","id":5,"image":"unit_6_part_5.png","pageorder":5,"story_id":5,"text":"Pop!","textposition":"topleft","updated_at":"2013-07-25T10:15:36Z"},
+
+{"audio":"module_1_unit_6_pop_2","created_at":"2013-05-24T13:57:11Z","id":12,"image":"unit_6_part_12.png","pageorder":12,"story_id":5,"text":"Pop!","textposition":"topleft","updated_at":"2013-07-25T10:17:09Z"},
+
+{"audio":"module_1_unit_6_the_dog_sat_on_the_pin","created_at":"2013-05-24T13:58:18Z","id":13,"image":"unit_6_part_11.png","pageorder":13,"story_id":5,"text":"The dog sat on the pin.","textposition":"topright","updated_at":"2013-07-25T10:17:17Z"}
 ];
